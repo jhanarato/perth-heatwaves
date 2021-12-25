23b2c01b3f00a467514237557874e132b8155c8f
Jhanarato <jhanarato@gmail.com>
2021-12-25 12:50:43 +0800

    Consecutive hot days have a heatwave_id value.

--- perth-heatwaves.R
+++ perth-heatwaves.R
@@ -20,7 +20,32 @@ temp_data <- read_csv("perth-temp.csv",
                       skip = 1)
 
 temp_data <- temp_data %>% 
-  na.omit()%>% 
+  na.omit() %>% 
   mutate(bom_date = make_date(year, month, day)) %>% 
   filter(max_temp >= 40) %>%
   select(bom_date, max_temp)
+
+# A heatwave can be defined as 2 or more days of 40C or over.
+# If the previous 40+ day is yesterday, or the next is tomorrow
+# then we're part of a heatwave.
+heatwaves <- temp_data %>%
+  mutate(
+    hot_yesterday = lag(bom_date) == bom_date - 1,
+    hot_tomorrow = lead(bom_date) == bom_date + 1,
+    heatwave = hot_yesterday | hot_tomorrow
+  ) %>%
+  na.omit() %>%
+  filter(heatwave) %>%
+  select(bom_date, max_temp)
+
+# Give each heatwave an identifier
+heatwaves <- heatwaves %>%
+  mutate(
+    increment = case_when(
+      is.na(lag(bom_date)) ~ TRUE,
+      lead(bom_date) - bom_date == 1 ~ FALSE,
+      lead(bom_date) - bom_date > 1 ~ TRUE
+    ),
+    heatwave_id = cumsum(increment)
+  ) %>%
+  select(-increment)
